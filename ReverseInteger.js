f6d3d9b71c6e597e3deaa2111a2eb1265b562537
jelimo-charity <charityjelimo893@gmail.com>
2024-02-19 15:23:05 +0300

    teach2give test

--- ReverseInteger.js
+++ ReverseInteger.js
@@ -1,21 +1,18 @@
-const reverseInteger = (num) => {
+const reverse_Integer = (num) => {
     // Convert the number to a string to handle individual digits
-    let numString = num.toString();
+    let numStr = num.toString();
 
     // Check if the number is negative
     let isNegative = num < 0;
 
     // Reverse the order of digits
-    let reversedString = numString.slice(isNegative ? 1 : 0).split('').reverse().join('');
+    let reversedStr = numStr.slice(isNegative ? 1 : 0).split('').reverse().join('');
 
     // Convert the string back to an integer and reapply the negative sign if necessary
-    let reversedNumber = isNegative ? -parseInt(reversedString) : parseInt(reversedString);
+    let reversedNumber = isNegative ? -parseInt(reversedStr) : parseInt(reversedStr);
 
     return reversedNumber;
 }
+console.log(reverse_Integer(-807)); 
+console.log(reverse_Integer(98));  
 
-// Examples
-console.log(reverseInteger(500));  // Output: 5
-console.log(reverseInteger(-56));  // Output: -65
-console.log(reverseInteger(-90));  // Output: -9
-console.log(reverseInteger(91));   // Output: 19
